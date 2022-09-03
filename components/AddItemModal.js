f16b3c5e96ae6a73a82cbaef8f5538a1e9cf461f
musembi77 <sammymusembi77@gmail.com>
2022-09-03 21:21:38 +0300

    handed over the site - adding admin credentials

--- components/AddItemModal.js
+++ components/AddItemModal.js
@@ -68,8 +68,8 @@ export function AddItemModal({isAddNewItemModalvisible,setIsAddNewItemModalModal
               const data = new FormData()
                 data.append("file", image);
                 data.append("upload_preset", "tryfashionsshop");
-                data.append("cloud_name", "tryfashionshop");
-                 axios.post("https://api.cloudinary.com/v1_1/tryfashionshop/image/upload",
+                data.append("cloud_name", "tryfashionshop2022");
+                 axios.post("https://api.cloudinary.com/v1_1/tryfashionshop2022/image/upload",
                   data).then((res)=>{
                     //console.log(res.data.url)
                     //console.log(res.data)
